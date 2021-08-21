751690fea2b0f7b533b2701603b5bbd30f725e66
example <example@example.com>
2021-08-21 11:24:35 +0900

    mysql config

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,18 @@ echo $HOSTNAME
 
 set -ex
 
+sudo systemctl daemon-reload
+
+echo "Restarting App"
+sudo systemctl restart isucondition.go.service
+
+echo "Restarting nginx"
 sudo cp -a ./conf/all/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 sudo cp -a ./conf/all/etc/nginx/sites-available/isucondition.conf /etc/nginx/sites-available/isucondition.conf
 sudo nginx -t &&  sudo systemctl restart nginx
 
-sudo systemctl daemon-reload
-sudo systemctl restart isucondition.go.service
+echo "Restarting mysql"
+sudo cp -a ./conf/etc/mysql/conf.d/my.cnf /etc/mysql/conf.d/my.cnf
+sudo cp -a ./conf/etc/mysql/conf.d/mysql.cnf /etc/mysql/conf.d/mysql.cnf
+sudo cp -a ./conf/etc/mysql/conf.d/mysqldump.cnf /etc/mysql/conf.d/mysqldump.cnf
+sudo systemctl restart mysqld

d6222bf61458e399020fb086ef854603ca087b54
wass80 <wasss80@gmail.com>
2021-08-21 13:31:24 +0900

    influx addr

--- influx/init.sh
+++ influx/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo drop
-curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=DROP DATABASE "isu"'
+curl -XPOST ${INFLUX_ADDR}'/query' --data-urlencode 'q=DROP DATABASE "isu"'
 echo create
-curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE "isu"'
+curl -XPOST ${INFLUX_ADDR}'/query' --data-urlencode 'q=CREATE DATABASE "isu"'

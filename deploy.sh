20fa4321b14be4502c86860c024e26814cefbf43
Your Name <you@example.com>
2021-08-21 12:35:13 +0900

    mysql only on 2

--- deploy.sh
+++ deploy.sh
@@ -25,22 +25,28 @@ sudo systemctl daemon-reload
 echo "Restarting App"
 pushd go
 go build
+sudo systemctl enable isucondition.go.service
 sudo systemctl restart isucondition.go.service
 popd
 
 echo "Restarting nginx"
 sudo cp -a ./conf/all/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 sudo cp -a ./conf/all/etc/nginx/sites-available/isucondition.conf /etc/nginx/sites-available/isucondition.conf
+sudo systemctl enable nginx
 sudo nginx -t &&  sudo systemctl restart nginx
 
-echo "Restarting mysql"
-sudo cp -a ./conf/all/etc/mysql/conf.d/my.cnf /etc/mysql/conf.d/my.cnf
-sudo cp -a ./conf/all/etc/mysql/conf.d/mysql.cnf /etc/mysql/conf.d/mysql.cnf
-sudo cp -a ./conf/all/etc/mysql/conf.d/mysqldump.cnf /etc/mysql/conf.d/mysqldump.cnf
-sudo systemctl restart mysqld
+if [[ "$INSTANCE_NUM" == 2 ]]; then
+  echo "Restarting mysql"
+  sudo cp -a ./conf/all/etc/mysql/conf.d/my.cnf /etc/mysql/conf.d/my.cnf
+  sudo cp -a ./conf/all/etc/mysql/conf.d/mysql.cnf /etc/mysql/conf.d/mysql.cnf
+  sudo cp -a ./conf/all/etc/mysql/conf.d/mysqldump.cnf /etc/mysql/conf.d/mysqldump.cnf
+  sudo systemctl enable mysql
+  sudo systemctl restart mysql
+fi
 
 if [[ "$INSTANCE_NUM" == 3 ]]; then
   echo "Restarting influxdb"
   sudo cp -a ./conf/$INSTANCE_NUM/etc/influxdb/influxdb.conf /etc/influxdb/influxdb.conf
+  sudo systemctl enable influxdb
   sudo systemctl restart influxdb
 fi

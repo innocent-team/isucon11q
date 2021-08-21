07a0eeeecbfbc9fe839da68a66fa48980e12ae6b
utgwkk <utagawakiki@gmail.com>
2021-08-21 11:55:16 +0900

    こうでした

--- get_isu_image.sh
+++ get_isu_image.sh
@@ -2,5 +2,5 @@
 set -ex
 
 cd icon
-echo "SELECT CONCAT('http://localhost:3000/api/isu/icon_for_devonly/', jia_isu_uuid) FROM isu" | sudo mysql -N -B isucondition | xargs wget
+echo "SELECT CONCAT('http://localhost:3000/api/icon_for_devonly/', jia_isu_uuid) FROM isu" | sudo mysql -N -B isucondition | xargs wget
 cd -

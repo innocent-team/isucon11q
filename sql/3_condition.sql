7890196ede0025412d9b5909fa334cce43d5a2f9
utgwkk <utagawakiki@gmail.com>
2021-08-23 23:16:56 +0900

    isu_conditionテーブルからid pkeyをなくして (jia_isu_uuid, timestamp) をpkeyにする (再チャレンジ)

--- sql/3_condition.sql
+++ sql/3_condition.sql
@@ -1,3 +1,18 @@
+CREATE TABLE `neo_isu_condition` (
+  `jia_isu_uuid` CHAR(36) NOT NULL,
+  `timestamp` DATETIME NOT NULL,
+  `is_sitting` TINYINT(1) NOT NULL,
+  `condition` VARCHAR(255) NOT NULL,
+  `message` VARCHAR(255) NOT NULL,
+  `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+
+  PRIMARY KEY (`jia_isu_uuid`, `timestamp`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+INSERT INTO `neo_isu_condition` SELECT `jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`, `created_at` FROM `isu_condition`;
+DROP TABLE `isu_condition`;
+RENAME TABLE `neo_isu_condition` TO `isu_condition`;
+
 ALTER TABLE `isu_condition` ADD (
     `is_broken` TINYINT(1) AS (`condition` REGEXP 'is_broken=true') STORED,
     `is_dirty` TINYINT(1) AS (`condition` REGEXP 'is_dirty=true') STORED,

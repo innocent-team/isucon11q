47fb91dece171757966ace2972ed0748ab22f500
utgwkk <utagawakiki@gmail.com>
2021-08-24 08:57:16 +0900

    field

--- go/main.go
+++ go/main.go
@@ -63,14 +63,15 @@ type Config struct {
 }
 
 type Isu struct {
-	ID         int       `db:"id" json:"id"`
-	JIAIsuUUID string    `db:"jia_isu_uuid" json:"jia_isu_uuid"`
-	Name       string    `db:"name" json:"name"`
-	Image      []byte    `db:"image" json:"-"`
-	Character  string    `db:"character" json:"character"`
-	JIAUserID  string    `db:"jia_user_id" json:"-"`
-	CreatedAt  time.Time `db:"created_at" json:"-"`
-	UpdatedAt  time.Time `db:"updated_at" json:"-"`
+	ID            int       `db:"id" json:"id"`
+	JIAIsuUUID    string    `db:"jia_isu_uuid" json:"jia_isu_uuid"`
+	Name          string    `db:"name" json:"name"`
+	Image         []byte    `db:"image" json:"-"`
+	Character     string    `db:"character" json:"character"`
+	JIAUserID     string    `db:"jia_user_id" json:"-"`
+	LastTimestamp time.Time `db:"last_condition_timestamp" json:"-"`
+	CreatedAt     time.Time `db:"created_at" json:"-"`
+	UpdatedAt     time.Time `db:"updated_at" json:"-"`
 }
 
 const isuColumnsForJSON = "id, jia_isu_uuid, name, `character`"

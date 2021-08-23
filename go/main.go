cccbf4864c59333e0faa0396c3b79daf4c94977d
utgwkk <utagawakiki@gmail.com>
2021-08-24 00:51:41 +0900

    客寄せパンダ

--- go/main.go
+++ go/main.go
@@ -1203,7 +1203,7 @@ func getTrend(c echo.Context) error {
 	}
 
 	// アプリケーション側でときどき Cache-Control: public, max-age=1 を付与する
-	cacheControlProbability := 0.1
+	cacheControlProbability := 0.05
 	if rand.Float64() <= cacheControlProbability {
 		c.Logger().Warnf("drop post isu condition request")
 		c.Response().Header().Add("Cache-Control", "public, max-age=1")
